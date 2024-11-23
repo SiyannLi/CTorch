249882f40b27d7b2d06cb5f8f4d0050a8ea80b84
Zhenjiang_Li_ubuntu24.04 <zhenjiang1.li@tum.de>
2024-11-23 14:12:28 +0100

    zhenjiang original

--- Layers/LinearLayer.cpp
+++ Layers/LinearLayer.cpp
@@ -3,3 +3,38 @@
 //
 
 #include "LinearLayer.h"
+
+void Init::xavier_uniform(std::vector<std::vector<double>>& weights, int fan_int, int fan_out)
+{
+    double limit = std::sqrt(6.0 / (fan_int + fan_out ));
+}
+
+void Init::kaiming_uniform(std::vector<std::vector<double>>& eights, int fan_in)
+{
+
+}
+
+void Init::constant(std::vector<double>& bias, double value)
+{
+
+}
+
+void Init::initialize_uniform(std::vector<std::vector<double>>& matrix, double min, double max)
+{
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<double> dist(min, max);  
+
+
+}
+
+LinearLayer::LinearLayer(int input_size, int out_size):
+input_size_(input_size), output_size_(out_size)
+{
+
+}
+
+std::vector<double> LinearLayer::forward(const std::vector<double>& input)
+{
+
+}
--- Layers/LinearLayer.h
+++ Layers/LinearLayer.h
@@ -1,13 +1,49 @@
 //
 // Created by siyan on 2024/10/27.
 //
-
 #ifndef LINEARLAYER_H
 #define LINEARLAYER_H
+
+#include <iostream>
+#include <vector>
+#include <random>
+#include <cassert>
+#include <cmath>
+
+
+class Init{
+public:
+    static void xavier_uniform(std::vector<std::vector<double>>& weights, int fan_int, int fan_out);
+    static void kaiming_uniform(std::vector<std::vector<double>>& eights, int fan_in);
+    static void constant(std::vector<double>& bias, double value);
+private:
+    static void initialize_uniform(std::vector<std::vector<double>>& matrix, double min, double max);
+};
 
 
 
 class LinearLayer {
+public:
+    /// @brief Constructor: initialize linear layer
+    /// @param input_size 
+    /// @param output_size 
+    LinearLayer(int input_size, int output_size);
+
+    /// @brief 
+    /// @param input 
+    /// @return 
+    std::vector<double> forward(const std::vector<double>& input);
+
+
+
+
+
+
+private:
+    int input_size_;                            // input size
+    int output_size_;                           // output size
+    std::vector<std::vector<double>> weights_;  // weights matrix
+    std::vector<float> bias_;                   // bias vector
 
 };
 
