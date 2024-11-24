dcd016b8c9ad93e5d9041553bccac86df7e9fc10
刘禹恒 <liuyuheng@liuyuhengdeMBP.fritz.box>
2024-11-24 02:50:36 +0100

    v1

--- Data/DataSet.h
+++ Data/DataSet.h
@@ -5,10 +5,39 @@
 #ifndef DATASET_H
 #define DATASET_H
 
-
+#include <vector>
+#include <fstream>
+#include <string>
 
 class DataSet {
+private:
+    std::vector<std::vector<double>> train_input;
+    std::vector<std::vector<double>> train_output;
+    std::vector<std::vector<double>> test_input;
+    std::vector<std::vector<double>> test_output;
+    std::vector<std::string> label;
 
+    void readMnistTrainImage();
+    void readMnistTrainLable();
+    void readMnistTestImage();
+    void readMnistTestLable();
+
+public:
+    DataSet();
+
+    void readMnistData();
+
+    void printDigit(std::vector<double>, double mask);
+
+    std::vector<std::vector<double>> getNormalizedData(std::vector<std::vector<double>>);
+
+    std::vector<std::vector<double>> getInput();
+    std::vector<std::vector<double>> getOutput();
+
+    std::vector<std::vector<double>> getTestInput();
+    std::vector<std::vector<double>> getTestOutput();
+
+    ~DataSet();
 };
 
 
---
+++ build/CMakeFiles/CTorch.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/liuyuheng/Downloads/CTorch/Data/DataLoader.cpp" "CMakeFiles/CTorch.dir/Data/DataLoader.cpp.o" "gcc" "CMakeFiles/CTorch.dir/Data/DataLoader.cpp.o.d"
+  "/Users/liuyuheng/Downloads/CTorch/Data/DataSet.cpp" "CMakeFiles/CTorch.dir/Data/DataSet.cpp.o" "gcc" "CMakeFiles/CTorch.dir/Data/DataSet.cpp.o.d"
+  "/Users/liuyuheng/Downloads/CTorch/DataType/Tensor.cpp" "CMakeFiles/CTorch.dir/DataType/Tensor.cpp.o" "gcc" "CMakeFiles/CTorch.dir/DataType/Tensor.cpp.o.d"
+  "/Users/liuyuheng/Downloads/CTorch/Layers/ConvolutionLayer.cpp" "CMakeFiles/CTorch.dir/Layers/ConvolutionLayer.cpp.o" "gcc" "CMakeFiles/CTorch.dir/Layers/ConvolutionLayer.cpp.o.d"
+  "/Users/liuyuheng/Downloads/CTorch/Layers/LinearLayer.cpp" "CMakeFiles/CTorch.dir/Layers/LinearLayer.cpp.o" "gcc" "CMakeFiles/CTorch.dir/Layers/LinearLayer.cpp.o.d"
+  "/Users/liuyuheng/Downloads/CTorch/Layers/ReLU.cpp" "CMakeFiles/CTorch.dir/Layers/ReLU.cpp.o" "gcc" "CMakeFiles/CTorch.dir/Layers/ReLU.cpp.o.d"
+  "/Users/liuyuheng/Downloads/CTorch/Optimizer/Adam.cpp" "CMakeFiles/CTorch.dir/Optimizer/Adam.cpp.o" "gcc" "CMakeFiles/CTorch.dir/Optimizer/Adam.cpp.o.d"
+  "/Users/liuyuheng/Downloads/CTorch/main.cpp" "CMakeFiles/CTorch.dir/main.cpp.o" "gcc" "CMakeFiles/CTorch.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
